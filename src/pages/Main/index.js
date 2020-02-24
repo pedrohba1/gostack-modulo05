52e8ccb77e075acbb0cead74409d598c3bc03623
Pedro <pedrohba18@gmail.com>
2020-02-24 11:44:36 -0300

    container componente, repo, e main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,11 @@
 /* eslint-disable react/no-unused-state */
 /* eslint-disable react/state-in-constructor */
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import { FaGithubAlt, FaSpinner, FaPlus } from 'react-icons/fa';
-import { Container, Form, SubmitButton } from './styles';
+import { Form, SubmitButton, List } from './styles';
+import Container from '../../components/Container';
 import api from '../../services/api';
 
 export default class Main extends Component {
@@ -12,6 +14,23 @@ export default class Main extends Component {
         repositories: [],
         loading: false,
     };
+
+    // carregar dados do localStorage
+    componentDidMount() {
+        const repositories = localStorage.getItem('repositories');
+
+        if (repositories) {
+            this.setState({ repositories: JSON.parse(repositories) });
+        }
+    }
+
+    // salva os dados em localStorage
+    componentDidUpdate(_, prevState) {
+        const { repositories } = this.state;
+        if (prevState.repositories !== repositories) {
+            localStorage.setItem('repositories', JSON.stringify(repositories));
+        }
+    }
 
     handleInputChange = e => {
         this.setState({ newRepo: e.target.value });
@@ -22,19 +41,32 @@ export default class Main extends Component {
 
         this.setState({ loading: true });
         const { newRepo, repositories } = this.state;
-        const res = await api.get(`/repos/${newRepo}`);
-        const data = {
-            name: res.data.full_name,
-        };
-        this.setState({
-            repositories: [...repositories, data],
-            newRepo: '',
-        });
+
+        try {
+            const res = await api.get(`/repos/${newRepo}`);
+            const data = {
+                name: res.data.full_name,
+            };
+            this.setState({
+                repositories: [...repositories, data],
+                newRepo: '',
+            });
+        } catch (error) {
+            if (error.response) {
+                /*
+                 * The request was made and the server responded with a
+                 * status code that falls out of the range of 2xx
+                 */
+            }
+            this.setState({ loading: false });
+            return;
+        }
+
         this.setState({ loading: false });
     };
 
     render() {
-        const { newRepo, loading } = this.state;
+        const { newRepo, loading, repositories } = this.state;
         return (
             <Container>
                 <h1>
@@ -57,6 +89,21 @@ export default class Main extends Component {
                         )}
                     </SubmitButton>
                 </Form>
+
+                <List>
+                    {repositories.map(repository => (
+                        <li key={repository.name}>
+                            <span>{repository.name}</span>
+                            <Link
+                                to={`/repository/${encodeURIComponent(
+                                    repository.name
+                                )}`}
+                            >
+                                Detalhes
+                            </Link>
+                        </li>
+                    ))}
+                </List>
             </Container>
         );
     }
--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,9 +1,59 @@
-import React from 'react';
+/* eslint-disable react/static-property-placement */
+/* eslint-disable react/state-in-constructor */
+/* eslint-disable react/prop-types */
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import api from '../../services/api';
 
-export default function Repository() {
-    return (
-        <div>
-            <h1>repo</h1>
-        </div>
-    );
+import Container from '../../components/Container';
+import { Loading } from './styles';
+
+class Repository extends Component {
+    static propTypes = {
+        match: PropTypes.shape({
+            params: PropTypes.shape({
+                repository: PropTypes.string,
+            }),
+        }).isRequired,
+    };
+
+    state = {
+        repository: {},
+        issues: [],
+        loading: true,
+    };
+
+    async componentDidMount() {
+        const { match } = this.props;
+
+        const repoName = decodeURIComponent(match.params.repository);
+
+        const [repository, issues] = await Promise.all([
+            api.get(`/repos/${repoName}`),
+            api.get(`/repos/${repoName}/issues`, {
+                params: {
+                    state: 'open',
+                    per_page: 5,
+                },
+            }),
+        ]);
+
+        this.setState({
+            repository: repository.data,
+            issues: issues.data,
+            loading: false,
+        });
+    }
+
+    render() {
+        const { repository, issues, loading } = this.state;
+
+        if (loading) {
+            return <Loading>Carregando</Loading>;
+        }
+
+        return <Container></Container>;
+    }
 }
+
+export default Repository;
